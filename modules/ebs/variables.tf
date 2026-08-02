d28661cc29f9356ee3c9fb7a8d9eff5c8f19cfbf
Andre <apak_mc@hotmail.de>
2026-08-02 16:25:48 +0000

    chore: apply tf fmt

--- modules/ebs/variables.tf
+++ modules/ebs/variables.tf
@@ -1,6 +1,6 @@
 variable "name_prefix" {
-    description = "Prefix used for resource names."
-    type = string
+  description = "Prefix used for resource names."
+  type        = string
 }
 
 variable "availability_zone" {
@@ -9,29 +9,29 @@ variable "availability_zone" {
 }
 
 variable "instance_id" {
-    description = "EC2 instance to which the EBS volume is attached." 
-    type        = string
+  description = "EC2 instance to which the EBS volume is attached."
+  type        = string
 }
 
 variable "volume_size" {
-    description = "Size of the persistent EBS volume in GiB"
-    type = number
-    default = 10
-    
-    validation {
-      condition = var.volume_size == 10
-      error_message = "The assignment requires a 10-GiB additional EBS volume."
-    }
+  description = "Size of the persistent EBS volume in GiB"
+  type        = number
+  default     = 10
+
+  validation {
+    condition     = var.volume_size == 10
+    error_message = "The assignment requires a 10-GiB additional EBS volume."
+  }
 }
 
 variable "device_name" {
-    description = "Requested Linux attachment name."
-    type = string
-    default = "/dev/sdf"
+  description = "Requested Linux attachment name."
+  type        = string
+  default     = "/dev/sdf"
 }
 
 variable "tags" {
-    description = "Tags applied to resources."
-    type = map(string)
-    default = {}
+  description = "Tags applied to resources."
+  type        = map(string)
+  default     = {}
 }
